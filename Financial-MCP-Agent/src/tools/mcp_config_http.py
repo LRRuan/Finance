9e552a54392bb362f2263d7a798d8bbb640478e1
LRRuan <lrruan0@outlook.com>
2025-11-14 16:22:22 +0800

    Update mcp_config_http.py

--- Financial-MCP-Agent/src/tools/mcp_config_http.py
+++ Financial-MCP-Agent/src/tools/mcp_config_http.py
@@ -4,7 +4,8 @@ MCP服务器配置模块 - 包含连接A股MCP服务器的配置信息
 
 SERVER_CONFIGS = {
     "a_share_mcp_v2": {
-        "transport": "streamable_http",         # 1. 将 transport 从 "stdio" 修改为 "tcp"
+        "transport": "streamable_http",
         "url": "http://127.0.0.1:8080/mcp"
     }
+
 }
